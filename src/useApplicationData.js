bd5afc3afcdc9bc1982bfdcf34a85dca073a6dba
Kevin Chu <kvnchu2@gmail.com>
2021-07-14 13:26:47 -0700

    fix events in combinedList

--- src/useApplicationData.js
+++ src/useApplicationData.js
@@ -42,45 +42,6 @@ const handleClick = function(eventDate){
 
     gapi.auth2.getAuthInstance().signIn()
     .then(() => {
-      
-      // var event = {
-      //   'summary': 'Awesome Event!',
-      //   'location': '800 Howard St., San Francisco, CA 94103',
-      //   'description': 'Really great refreshments',
-      //   'start': {
-      //     'dateTime': '2020-06-21T09:00:00-07:00',
-      //     'timeZone': 'America/Los_Angeles'
-      //   },
-      //   'end': {
-      //     'dateTime': '2020-06-21T17:00:00-07:00',
-      //     'timeZone': 'America/Los_Angeles'
-      //   },
-      //   'recurrence': [
-      //     'RRULE:FREQ=DAILY;COUNT=2'
-      //   ],
-      //   'attendees': [
-      //     {'email': 'lpage@example.com'},
-      //     {'email': 'sbrin@example.com'}
-      //   ],
-      //   'reminders': {
-      //     'useDefault': false,
-      //     'overrides': [
-      //       {'method': 'email', 'minutes': 24 * 60},
-      //       {'method': 'popup', 'minutes': 10}
-      //     ]
-      //   }
-      // }
-
-      // var request = gapi.client.calendar.events.insert({
-      //   'calendarId': 'primary',
-      //   'resource': event,
-      // })
-
-      // request.execute(event => {
-      //   console.log(event)
-      //   window.open(event.htmlLink)
-      // })
-
       let splitDate = eventDate.split(" ")
       const maxDay = Number(eventDate.split(" ")[0]) + 1;
       splitDate[0] = maxDay;
@@ -103,26 +64,38 @@ const handleClick = function(eventDate){
         // setEvents(eventsObject);
         return eventsObject
       }).then((eventsObject) => {
-        let eventsArr = [];
+        let icbcArr = [];
+        let wsbcArr = [];
         console.log("eventsObject", eventsObject)
           eventsObject.forEach(event => {
             const eventSplit = event.location
-            console.log(eventSplit)
-            eventsArr.push(eventSplit);
+            const eventSummary = event.summary
+            console.log(eventSummary.split(" "));
+            if (eventSummary.split(" ")[0] === "ICBC" || eventSummary === "home") {
+              const icbcObj = {}
+              icbcObj["location"] = eventSplit;
+              icbcObj["summary"] = eventSummary;
+              icbcArr.push(icbcObj)
+            } else {
+              wsbcArr.push(eventSplit)
+            }
           })
-          return Promise.all( eventsArr.map(event => axios.get(`https://api.tomtom.com/search/2/structuredGeocode.json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&countryCode=CA&postalCode=${event}`)
+
+          console.log("icbcArr", icbcArr)
+          return Promise.all( icbcArr.map(event => axios.get(`https://api.tomtom.com/search/2/structuredGeocode.json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv&countryCode=CA&postalCode=${event.location}`)
             .then(({data})=> data))
-          )
+        )
             .then((coordinates) => {
+              console.log("coordinates", coordinates);
+
               for(let y = 0; y < coordinates.length; y++) {
-                console.log("coordinates", coordinates);
                 if (y !== coordinates.length - 1) {
                   axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=atFqCv6vs5HzL0u9qS9G5HXnhdYAA6kv`)
                     .then((result) => {
                       const travelMileageObj = {};
                       travelMileageObj["mileage"] = result.data.routes[0].summary.lengthInMeters;
                       travelMileageObj["traveltime"] = result.data.routes[0].summary.travelTimeInSeconds;
-                      travelMileageObj["events"] = eventsObject[y];
+                      travelMileageObj["events"] = icbcArr[y];
                       travelMileageObj["order"] = y;
                       
                       setRoutes(prevState => ([...prevState, travelMileageObj]));
@@ -133,7 +106,7 @@ const handleClick = function(eventDate){
                     })
                 } else {
                   const eventsObj = {};
-                  eventsObj["events"] = eventsObject[y];
+                  eventsObj["events"] = icbcArr[y];
                   eventsObj["order"] = y;
                   setRoutes(prevState => ([...prevState, eventsObj]));
                 }
