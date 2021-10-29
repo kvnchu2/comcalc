bbcd3453382625d534cb5247fb56e18a62cd970d
Kevin Chu <kvnchu2@gmail.com>
2021-10-28 20:35:50 -0700

    add show component in results

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,42 +1,10 @@
 import React from "react";
+import Show from "./show.js"
 
 export default function Results(props) {
   return (
     <>
-    <section id="search-results">
-      <table class="results-table">
-        <thead>
-          <tr>
-            <th scope="col">Client</th>
-            <th scope="col">Travel Time/Duration</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.combinedList}
-          {props.combinedListTwo}
-        </tbody>
-      </table>
-    </section>
-    <section id="total-travel-calculation">
-      <label>Total Travel Time</label>
-      <div>
-        {props.travelTime} seconds
-      </div>
-      <label>Total Mileage</label>
-      <div>
-        {props.mileage} meters
-      </div>
-    </section>
-    <section id="travel-calculation">
-      <label>Travel Time per Client</label>
-      <div>
-        {((props.travelTime/((props.routes.length + props.routesTwo.length) - 2))/60)/60}
-      </div>
-      <label>Mileage per Client</label>
-      <div>
-        {((props.mileage/((props.routes.length + props.routesTwo.length) - 2))/1000)}
-      </div>
-    </section>
+    <Show combinedList={props.combinedList} combinedListTwo={props.combinedListTwo} travelTime={props.travelTime} mileage={props.mileage} routes={props.routes} routesTwo={props.routesTwo}/>
     </>
   )
 };
