3dc0076d0da2f1c80f935b7598ce4aa76ec9d7ed
Kevin Chu <kvnchu2@gmail.com>
2021-10-30 10:46:33 -0700

    add loading component

---
+++ src/components/results/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Loading(props) {
+  return (
+    <>
+    <section id="search-results">
+      <p>Loading results...</p>
+    </section>
+    </>
+  )
+};
