23a6fee1b17f98ed01aa9f0aa76920d38e094272
Kevin Chu <kvnchu2@gmail.com>
2021-12-11 11:30:41 -0800

    add quotes to react app tom tom key variable env variable

--- src/useApplicationData.js
+++ src/useApplicationData.js
@@ -40,7 +40,7 @@ const calculateICBCRouteTwo = (coordinates, icbcArr) => {
   console.log("icbcArrTwo", icbcArr)
   for(let y = 0; y < coordinates.length; y++) {
     if (y !== coordinates.length - 1) {
-      axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=${process.env.REACT_APP_TOM_TOM_KEY}&departAt=${icbcArr[y].startTime}&traffic=true`)
+      axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=${process.env.["REACT_APP_TOM_TOM_KEY"]}&departAt=${icbcArr[y].startTime}&traffic=true`)
         .then((result) => {
           const travelMileageObj = {};
           travelMileageObj["mileage"] = result.data.routes[0].summary.lengthInMeters;
@@ -72,7 +72,7 @@ const calculateICBCRoute = (coordinates, icbcArr) => {
   console.log("sample coordinates", coordinates)
   for(let y = 0; y < coordinates.length; y++) {
     if (y !== coordinates.length - 1) {
-      axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=${process.env.REACT_APP_TOM_TOM_KEY}&departAt=${icbcArr[y].startTime}&traffic=true`)
+      axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${coordinates[y].results[0].position.lat},${coordinates[y].results[0].position.lon}:${coordinates[y+1].results[0].position.lat},${coordinates[y+1].results[0].position.lon}/json?key=${process.env["REACT_APP_TOM_TOM_KEY"]}&departAt=${icbcArr[y].startTime}&traffic=true`)
         .then((result) => {
           const travelMileageObj = {};
           travelMileageObj["mileage"] = result.data.routes[0].summary.lengthInMeters;
