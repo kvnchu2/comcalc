2da168e6943cdd648239e31d9c65e1e694be96bd
Kevin Chu <kvnchu2@gmail.com>
2021-12-16 18:55:32 -0800

    add react router dom and reorganize contents of App.js into home component

---
+++ src/components/home.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './home.css';
+import DateInput from "./DateInput.js"
+import Results from "./results/index.js"
+import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/styles";
+import useApplicationData from '../useApplicationData.js';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      // margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  textfield: {
+    width: "100%",
+  },
+}));
+
+export default function Home() {
+
+  const { routesTwo, travelTime, routes, events, inputDate, handleSearchInput, handleIcbcSubmit, handleWsbcSubmit, mileage, results } = useApplicationData();
+  const classes = useStyles();
+
+  const combinedList = routes.sort(function(a,b) { return a.order - b.order}).map( route => {
+    if (isNaN(route.traveltime)) {
+      return (
+        <tr>
+          <td>{route.events.summary}</td>
+        </tr>
+      )
+    } else {
+      return (
+        <tr>
+          <td>{route.events.summary}</td>
+          <td>{Math.ceil(route.traveltime / 60)} minutes {Math.ceil(route.mileage / 1000)} kilometers</td>
+        </tr>
+      )
+    }
+  })
+
+  const combinedListTwo = routesTwo.sort(function(a,b) { return a.order - b.order}).map( route => {
+    if (isNaN(route.traveltime)) {
+      return (
+        <tr>
+          <td>{route.events.summary}</td>
+        </tr>
+      )
+    } else {
+      return (
+        <tr>
+          <td>{route.events.summary}</td>
+          <td>{Math.ceil(route.traveltime / 60)} minutes {Math.ceil(route.mileage / 1000)} kilometers</td>
+        </tr>
+      )
+    }
+  })
+
+ 
+
+  return (
+    <div className="App">
+      <header class="App-header">
+        <section id="search-panel">
+          <DateInput inputDate={inputDate} handleSearchInput={handleSearchInput}></DateInput>
+          <div id="button-section">
+          <Button variant="contained" color="primary" style={{width: 100, height: 50}} onClick={handleIcbcSubmit}>
+            Calculate ICBC
+          </Button>
+          <Button variant="contained" color="secondary" style={{width: 100, height: 50}} onClick={handleWsbcSubmit} >
+            Calculate WSBC
+          </Button>
+          </div>
+        </section>
+        <Results results={results} combinedList={combinedList} combinedListTwo={combinedListTwo} travelTime={travelTime} mileage={mileage} routes={routes} routesTwo={routesTwo}/>
+      </header>
+    </div>
+  );
+}
