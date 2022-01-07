a77b18d9e34c0fbd224bc98dfbc3b2ad7e86725f
Kevin Chu <kvnchu2@gmail.com>
2022-01-06 20:29:10 -0800

    add styling for navbar elements

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./navbar.css";
 import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/clients">Clients</Link>
+    <div id="navbar">
+      <Link class="navbar-link" to="/">Home</Link>
+      <Link class="navbar-link" to="/clients">Clients</Link>
     </div>
   )
 };
