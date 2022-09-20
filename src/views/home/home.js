702f3303a5e9d0bc78548736d39e2310f40be16c
Kevin Chu <kvnchu2@gmail.com>
2022-09-19 23:00:11 -0700

    add promise.all in useeffect of home.js

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -64,15 +64,27 @@ export default function Home() {
   })
 
   useEffect(() => {
-    axios.get("https://travel-calculator-server.herokuapp.com/client/find/enddate")
-      .then((result) => {
-        console.log(result.data.rows);
-        setClientEndDate(result.data.rows);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+
+    Promise.all([
+      axios.get("https://travel-calculator-server.herokuapp.com/client/find/enddate")
+    ]).then((all) => {
+      
+      setClientEndDate(all[0].data.rows);
+     
+    });
   }, []);
+
+  // useEffect(() => {
+
+  //   Promise.all([
+  //     axios.get("https://travel-calculator-server.herokuapp.com/client/find/enddate"),
+  //     axios.get('http://localhost:8001/api/appointments'),
+  //     axios.get('http://localhost:8001/api/interviewers'),
+  //   ]).then((all) => {
+      
+  //     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+  //   });
+  // }, [])
 
   return (
     <div className="App">
